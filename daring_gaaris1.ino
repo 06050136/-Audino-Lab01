91f3876be5877800c2438f1a2a69cd219c99b1f8
06050136 <47806931+06050136@users.noreply.github.com>
2019-02-20 15:08:49 +0800

    Add files via upload

---
+++ daring_gaaris1.ino
@@ -0,0 +1,55 @@
+void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);//short
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, LOW);
+  delay(100);
+  
+  digitalWrite(9, HIGH); //long
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, LOW);
+  delay(100);
+  
+  digitalWrite(9, HIGH); //short
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, LOW);
+  delay(300);
+}
